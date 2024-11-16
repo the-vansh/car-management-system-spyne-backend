0a0b0115fc5fe25b68aafe021920d452d3bf5887
vansh <contactvanshsaini@gmail.com>
2024-11-16 19:00:43 +0530

    cors policy

--- apis.js
+++ apis.js
@@ -21,16 +21,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://car-management-system-spyne.vercel.app");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-        res.sendStatus(204); // No Content
-    } else {
-        next();
-    }
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "https://car-management-system-spyne.vercel.app");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     if (req.method === "OPTIONS") {
+//         res.sendStatus(204); // No Content
+//     } else {
+//         next();
+//     }
+// });
 
 // Connect to MongoDB
 connectDB();
